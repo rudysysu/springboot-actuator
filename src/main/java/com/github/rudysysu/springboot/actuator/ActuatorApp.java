16802bfe7c56bbc8d5b1f738eefe4158e0b09b2b
dingyong2 <dingyong2@yy.com>
2022-01-05 18:49:07 +0800

    update

--- src/main/java/com/github/rudysysu/springboot/actuator/ActuatorApp.java
+++ src/main/java/com/github/rudysysu/springboot/actuator/ActuatorApp.java
@@ -1,4 +1,4 @@
-package com.dy.actuator;
+package com.github.rudysysu.springboot.actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
